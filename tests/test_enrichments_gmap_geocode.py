12a408b66435ca9cb5598407a7bda30bf05502d1
antigravkids <hcarter333@gmail.com>
2023-12-22 20:55:06 -0800

    adds tests

---
+++ tests/test_enrichments_gmap_geocode.py
@@ -0,0 +1,92 @@
+from datasette.app import Datasette
+from datasette_enrichments.utils import wait_for_job
+import pytest
+import re
+import sqlite_utils
+#originally created as https://github.com/datasette/datasette-enrichments-opencage/tree/6249f12d46b915e0070eb14ac19f4e66a311776a/tests
+#by Simon Willison
+
+@pytest.fixture
+def non_mocked_hosts():
+    # This ensures httpx-mock will not affect Datasette's own
+    # httpx calls made in the tests by datasette.client:
+    return ["localhost"]
+
+
+@pytest.mark.asyncio
+@pytest.mark.parametrize("api_key_from_config", (True, False))
+@pytest.mark.parametrize("store_json_column", (True, False))
+async def test_enrichment(tmpdir, api_key_from_config, store_json_column, httpx_mock):
+    #New structure: ["geometry"]["location"]["lat"]
+    httpx_mock.add_response(
+        url=re.compile(r"https://maps.googleapis.com/maps/api/geocode/json.*"),
+        method="GET",
+        json={
+            "results": [
+                {
+                    "geometry": {
+                            "location":{
+                                "lat": 38.8976633,
+                                "lng": -77.0365739,
+                            }
+                    }
+                }
+            ]
+        },
+    )
+    db_path = str(tmpdir / "data.db")
+    db = sqlite_utils.Database(db_path)
+    db["addresses"].insert(
+        {"id": 1, "address": "1600 Pennsylvania Ave NW, Washington, DC 20500"},
+        pk="id",
+    )
+
+    metadata = {}
+    if api_key_from_config:
+        metadata["plugins"] = {"enrichments-gmap-geocode": {"api_key": "abc123"}}
+    datasette = Datasette([db_path], metadata=metadata)
+
+    cookies = {"ds_actor": datasette.sign({"a": {"id": "root"}}, "actor")}
+    csrftoken = (
+        await datasette.client.get("/-/enrich/data/addresses/gm_api_geocoder", cookies=cookies)
+    ).cookies["ds_csrftoken"]
+    cookies["ds_csrftoken"] = csrftoken
+
+    post = {
+        "input": "{{ address }}",
+        "csrftoken": cookies["ds_csrftoken"],
+    }
+    if not api_key_from_config:
+        post["api_key"] = "abc123"
+    if store_json_column:
+        post["json_column"] = "details"
+
+    response = await datasette.client.post(
+        "/-/enrich/data/addresses/gm_api_geocoder",
+        data=post,
+        cookies=cookies,
+    )
+    assert response.status_code == 302
+
+    job_id = response.headers["Location"].split("=")[-1]
+    await wait_for_job(datasette, job_id, timeout=1)
+
+    expected_columns = {
+        "id": int,
+        "address": str,
+        "latitude": float,
+        "longitude": float,
+    }
+    if store_json_column:
+        expected_columns["details"] = str
+
+    assert db["addresses"].columns_dict == expected_columns
+
+    # Check the API key was used
+    request = httpx_mock.get_request()
+    assert request.url.params["key"] == "abc123"
+
+    if not store_json_column:
+        assert request.url.params["no_annotations"] == "1"
+    else:
+        assert "no_annotations" not in request.url.params
